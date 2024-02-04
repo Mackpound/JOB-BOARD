1d2e38b3a0aecbaf90542418b2f2b32b0df73572
Mackpound <igiliegbepatrick@gmail.com>
2024-02-04 09:20:26 +0100

    second commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,3 +6,34 @@ var buttons = document.querySelectorAll("button")
             button.classList.toggle('isClicked');
         }
     })
+
+
+// const jobBoxContent = document.getElementsByClassName("job-box-content")[0]
+const jobNames = document.querySelectorAll(".job-name")
+const jobView = document.querySelectorAll("#job-view")
+
+for (let i = 0; i < jobNames.length; i++){
+    const jobName = jobNames[i];
+
+
+    jobName.addEventListener("mouseover", () =>{
+        if(jobName.innerText.value <= 15){
+           return true;
+        }else{
+            const jobParent = jobName.parentElement;
+            const popName = jobParent.getElementsByClassName("job-view")[0]
+            popName.classList.add("active")
+            popName.textContent = jobName.textContent;
+            setTimeout(() =>{
+                popName.classList.remove("active")
+            }, 1700)
+        }
+    })
+}
+
+const applyBtn = document.querySelectorAll(".job-apply-btn > button")
+      applyBtn.forEach(btn => {
+        btn.addEventListener("click", function(){
+            btn.innerHTML = `<span style="white-space:nowrap;"><i class="fa fa-check"></i>&nbsp;Applied</span> `
+        })
+      })
